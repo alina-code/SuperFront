360c40e2c24ca7a6f2607034a0de3c4a2bb9be3f
Nastia Nemyria <anasta5ukr@gmail.com>
2021-04-22 18:12:04 +0300

    delete table row on page (needs testing)

--- public/javascripts/delition.js
+++ public/javascripts/delition.js
@@ -3,8 +3,11 @@ function deleteCategory(category_id){
 
     fetch(uri).then(response => response.json())
         .then(data => {
-            if(data==1) alert('Delition was sucessful');
-            else alert('Delition failed');
+            if(data==1){
+                alert('Delition was sucessful');
+                document.getElementById(category_id).remove();
+            }
+            else{alert('Delition failed');}
             document.getElementById('table1').innerHTML = buildTableFromJson(data);
         })
         .catch(err => console.log(err));
