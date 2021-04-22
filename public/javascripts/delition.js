3f0e0376c511a2323c66085b33e1db5e23e8af6c
Nastia Nemyria <anasta5ukr@gmail.com>
2021-04-22 18:01:32 +0300

    delition changes

--- public/javascripts/delition.js
+++ public/javascripts/delition.js
@@ -3,6 +3,8 @@ function deleteCategory(category_id){
 
     fetch(uri).then(response => response.json())
         .then(data => {
+            if(data==1) alert('Delition was sucessful');
+            else alert('Delition failed');
             document.getElementById('table1').innerHTML = buildTableFromJson(data);
         })
         .catch(err => console.log(err));
